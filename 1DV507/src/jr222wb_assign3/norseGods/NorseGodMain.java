3dfe510224f71662aa4db1d196f6eefc409162ff
spender <spender@eddie>
2018-03-07 20:14:18 +0100

    Almost done

--- 1DV507/src/jr222wb_assign3/norseGods/NorseGodMain.java
+++ 1DV507/src/jr222wb_assign3/norseGods/NorseGodMain.java
@@ -3,22 +3,73 @@ package jr222wb_assign3.norseGods;
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class NorseGodMain extends Application {
 	private static ArrayList<NorseGod> gods = new ArrayList<NorseGod>();
-	
+
 	public void start(Stage primaryStage) {
-		HBox box = new HBox();
+		BorderPane root = new BorderPane();	//Main box	
+		root.setPadding(new Insets(10));
 		
+
+		GridPane listAndContent = new GridPane(); //Grid for the list and content
+
+		VBox content = new VBox(); //Box with content
+		content.setSpacing(5);
+
+		Label header = new Label("Norse Gods and Other Beings"); //Header over list and content
+		header.setFont(new Font(30));
+		
+
 		//Create ListView with god names
 		ListView<String> godList = new ListView<String>(); //New ListView
-		godList.setPrefWidth(200);	//Set width
+		godList.setPrefWidth(150);	//Set width
 		for (NorseGod g : gods) { godList.getItems().add(g.getName()); } //Fill with name of gods from list
+
+		//Create the necessary labels for content
+		Label name = new Label();
+		name.setFont(new Font(18));
+		Label race = new Label();
+		race.setFont(new Font(15));
+		Label desc = new Label();
+		desc.setWrapText(true);
+		desc.setTextAlignment(TextAlignment.JUSTIFY);
+		content.getChildren().addAll(name, race, desc);
+		ScrollPane scroll = new ScrollPane(content);
+		scroll.setFitToWidth(true);
+		
+		//Add list and content to pane
+		listAndContent.add(godList,0,0);
+		listAndContent.add(content, 1, 0);
+		
+		//Add nodes to root
+		root.setTop(header);
+		root.setLeft(godList);
+		root.setCenter(scroll);
+
+		godList.getSelectionModel().selectedIndexProperty().addListener(ov	-> { 
+			//showJedi.setImage(manyJedi.get(jediList.getSelectionModel().getSelectedIndex()));
+			name.setText(gods.get(godList.getSelectionModel().getSelectedIndex()).getName());
+			race.setText(gods.get(godList.getSelectionModel().getSelectedIndex()).getRace());
+			desc.setText(gods.get(godList.getSelectionModel().getSelectedIndex()).getDesc());
+		});
+
+		Scene scene = new Scene(root, 640, 480);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Norse Gods and Other Beings");
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		//Create gods and add to list
 		gods.add(new NorseGod("Thor", "Aesir", "In Norse mythology, Thor (/θɔːr/; from Old Norse Þórr) is a hammer-wielding god associated with thunder,"
@@ -28,7 +79,7 @@ public class NorseGodMain extends Application {
 				+ " of the Germanic peoples, from the Roman occupation of regions of Germania, to the tribal expansions of the Migration Period, to"
 				+ " his high popularity during the Viking Age, when, in the face of the process of the Christianization of Scandinavia, emblems of his"
 				+ " hammer, Mjölnir, were worn and Norse pagan personal names containing the name of the god bear witness to his popularity."));
-		
+
 		gods.add(new NorseGod("Sif", "Aesir", "In Norse mythology, Sif is a goddess associated with earth. Sif is attested in the Poetic Edda,"
 				+ " compiled in the 13th century from earlier traditional sources, and the Prose Edda, written in the 13th century by Snorri Sturluson,"
 				+ " and in the poetry of skalds. In both the Poetic Edda and the Prose Edda, Sif is the wife of the thunder god Thor and is known for"
@@ -37,7 +88,7 @@ public class NorseGodMain extends Application {
 				+ " headpiece made for Sif, resulting in not only Sif's golden tresses but also five other objects for other gods.\n\nScholars have"
 				+ " proposed that Sif's hair may represent fields of golden wheat, that she may be associated with fertility, family, wedlock and/or"
 				+ " that she is connected to rowan, and that there may be an allusion to her role or possibly her name in the Old English poem Beowulf."));
-		
+
 		gods.add(new NorseGod("Odin", "Aesir", "In Old Norse texts, Odin is depicted as one-eyed and long-bearded, frequently wielding a spear named"
 				+ " Gungnir, and wearing a cloak and a broad hat. He is often accompanied by his animal companions and familiars—the wolves Geri and"
 				+ " Freki and the ravens Huginn and Muninn, who bring him information from all over Midgard—and rides the flying, eight-legged steed"
@@ -52,7 +103,7 @@ public class NorseGodMain extends Application {
 				+ " wise being Mímir for advice, and during the foretold events of Ragnarök, Odin is told to lead the einherjar into battle before being"
 				+ " consumed by the monstrous wolf Fenrir. In later folklore, Odin appears as a leader of the Wild Hunt, a ghostly procession of the"
 				+ " dead through the winter sky. He is associated with charms and other forms of magic, particularly in Old English and Old Norse texts."));
-		
+
 		gods.add(new NorseGod("Frigg", "Aesir", "In Germanic mythology, Frigg (Old Norse), Frija (Old High German), Frea (Langobardic), and Frige"
 				+ " (Old English) is a goddess. In nearly all sources, she is described as the wife of the god Odin. In Old High German and Old Norse"
 				+ " sources, she is also connected with the goddess Fulla. The English weekday name Friday (etymologically Old English \"Frīge's day\")"
@@ -62,7 +113,7 @@ public class NorseGodMain extends Application {
 				+ " associated with the Earth, otherwise personified as an apparently separate entity Jörð (Old Norse \"Earth\"). The children of Frigg"
 				+ " and Odin include the gleaming god Baldr. Due to significant thematic overlap, scholars have proposed a particular connection to the"
 				+ " goddess Freyja."));
-		
+
 		gods.add(new NorseGod("Baldr", "Aesir", "Baldr (also Balder, Baldur) is a Æsir god of light, joy, purity, and the summer sun in Norse mythology,"
 				+ " and a son of the god Odin and the goddess Frigg. He is the father of Forseti, and He has numerous brothers, such as Thor and Váli."
 				+ "\n\nIn the 12th century, Danish accounts by Saxo Grammaticus and other Danish Latin chroniclers recorded a euhemerized account of"
@@ -72,7 +123,7 @@ public class NorseGodMain extends Application {
 				+ " of his character.\n\nAccording to Gylfaginning, a book of Snorri Sturluson's Prose Edda, Baldr's wife is Nanna and their son is"
 				+ " Forseti. In Gylfaginning, Snorri relates that Baldr had the greatest ship ever built, Hringhorni, and that there is no place more"
 				+ " beautiful than his hall, Breidablik."));
-		
+
 		gods.add(new NorseGod("Loki", "Aesir", "Loki (Old Norse [ˈloki], Modern Icelandic [ˈlɔːkɪ], often Anglicized as /ˈloʊki/) is a god in Norse"
 				+ " mythology. Loki is in some sources the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By the jötunn"
 				+ " Angrboða, Loki is the father of Hel, the wolf Fenrir, and the world serpent Jörmungandr. By his wife Sigyn, Loki is the father of"
@@ -87,6 +138,8 @@ public class NorseGodMain extends Application {
 				+ " she must empty the bowl when it is full, and the venom that drips in the meantime causes Loki to writhe in pain, thereby causing"
 				+ " earthquakes. With the onset of Ragnarök, Loki is foretold to slip free from his bonds and to fight against the gods among the"
 				+ " forces of the jötnar, at which time he will encounter the god Heimdallr and the two will slay each other."));
-		
+
+		launch(args);
+
 	}
 }
