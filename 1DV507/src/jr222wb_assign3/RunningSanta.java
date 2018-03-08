dc2b45b59852d5381739bd34cf8ad996c2aaa55d
spender <spender@eddie>
2018-03-08 17:06:10 +0100

    Santa's in town!

---
+++ 1DV507/src/jr222wb_assign3/RunningSanta.java
@@ -0,0 +1,85 @@
+package jr222wb_assign3;
+
+import java.io.FileInputStream;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class RunningSanta extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Group root = new Group();
+		
+		//Load all images
+		ImageView background = new ImageView(new Image(new FileInputStream("src/jr222wb_assign3/images/bg.png")));		
+		Image run1 = new Image(new FileInputStream("src/jr222wb_assign3/images/run1.png"));
+		Image run2 = new Image(new FileInputStream("src/jr222wb_assign3/images/run2.png"));
+		Image run3 = new Image(new FileInputStream("src/jr222wb_assign3/images/run3.png"));
+		Image run4 = new Image(new FileInputStream("src/jr222wb_assign3/images/run4.png"));
+		Image run5 = new Image(new FileInputStream("src/jr222wb_assign3/images/run5.png"));
+		Image run6 = new Image(new FileInputStream("src/jr222wb_assign3/images/run6.png"));
+		Image run7 = new Image(new FileInputStream("src/jr222wb_assign3/images/run7.png"));
+		Image run8 = new Image(new FileInputStream("src/jr222wb_assign3/images/run8.png"));
+		Image run9 = new Image(new FileInputStream("src/jr222wb_assign3/images/run9.png"));
+		Image run10 = new Image(new FileInputStream("src/jr222wb_assign3/images/run10.png"));
+		Image run11 = new Image(new FileInputStream("src/jr222wb_assign3/images/run11.png"));
+		
+		//Set properties for displaying Santa
+		ImageView santa = new ImageView(run1); 
+		santa.setSmooth(true);
+		santa.setFitHeight(150);
+		santa.setPreserveRatio(true);
+		santa.setY(300);
+		santa.setX(20);
+		
+		//Add everything to scene and stage
+		root.getChildren().addAll(background, santa);
+		Scene scene = new Scene(root, 1024, 537);
+		primaryStage.setTitle("Deserted Santa!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		//Array for keeping track of images
+		Image[] frames = {run1, run2, run3, run4, run5, run6, run7, run8, run9, run10, run11};
+		
+		//Counter for
+		Count count = new Count();
+		KeyFrame k = new KeyFrame(Duration.millis(100), e -> {
+			santa.setImage(frames[count.get()]); //Set new frame based on count value
+			if (count.get() < frames.length - 1) //Each time run, increment count if last value of array is not reached				
+				count.inc();			
+			else 
+				count.set(0); //Else start over
+			
+			santa.setX(santa.getX() + 20); //Move Santa 20 pixels each cycle			
+		});		
+		
+		//Create timeline and set number of cycles
+		Timeline t = new Timeline(k);
+		t.setCycleCount(43);
+		t.play();
+		
+	}
+	//Container class to set data from anonymous method
+	private static class Count{
+		private int count = 0;			
+		public void set(int n) { count = n; }
+		public void inc() { count++; }
+		public int get() { return count; }
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+
+}
