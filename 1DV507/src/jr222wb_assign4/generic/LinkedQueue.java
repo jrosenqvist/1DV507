70c04d25265ff219f8895c9fd248b35d3e3832f3
Joakim Rosenqvist <joakim.rosenqvist@gmail.com>
2018-03-12 20:56:10 +0100

    New classes

---
+++ 1DV507/src/jr222wb_assign4/generic/LinkedQueue.java
@@ -0,0 +1,49 @@
+package jr222wb_assign4.generic;
+
+import java.util.Iterator;
+
+public class LinkedQueue<T> implements Queue<T> {
+
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public void enqueue(T t) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Object first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Object last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ 1DV507/src/jr222wb_assign4/generic/Queue.java
@@ -0,0 +1,14 @@
+package jr222wb_assign4.generic;
+
+import java.util.Iterator;
+
+public interface Queue<T> {
+	public int size();                     // current queue size 
+	public boolean isEmpty();              // true if queue is empty 
+	public void enqueue(T t);			   // add element at end of queue 
+	public Object dequeue();               // return and remove first element. 
+	public Object first();                 // return (without removing) first element 
+	public Object last();                  // return (without removing) last element 
+	public String toString();              // return a string representation of the queue content
+	public Iterator<T> iterator();		   // element iterator
+}
