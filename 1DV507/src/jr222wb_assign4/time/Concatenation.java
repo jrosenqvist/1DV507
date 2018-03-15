46ee3ca2f8dd19cbfe59948dd74e5833d5b0fe9c
Joakim Rosenqvist <joakim.rosenqvist@gmail.com>
2018-03-15 04:09:59 +0100

    Fixes and additions

---
+++ 1DV507/src/jr222wb_assign4/time/Concatenation.java
@@ -0,0 +1,112 @@
+package jr222wb_assign4.time;
+
+public class Concatenation {
+	public static void main(String[] args) {
+		final int RUNS = 5; //Number of test cycles
+		int[][] results = new int[4][2 * RUNS]; //Store results from each cycle
+		long before, current = 0; //Variables for time-keeping
+		int count; //Counter for tests
+		String str; //String for concatenation tests
+
+		//Warm-up, make sure everything is properly initialized before running real tests
+		before = System.currentTimeMillis();
+		str = "";
+		while (current < 2000) {
+			current = System.currentTimeMillis() - before;
+			str += "0";
+		}
+		
+		//Tests
+		for (int i = 0; i < RUNS; i++) {
+			System.out.println("Starting run " + (i + 1));
+			int index = 2 * i; //Index used for storing results
+
+			//Plus operator, short string
+			count = 0;
+			str = "";
+			current = 0;
+			before = System.currentTimeMillis();			
+			while (current <= 1000) {				
+				str += "0";
+				count++;
+				current = System.currentTimeMillis() - before;
+			}
+			results[0][index] = count;
+			results[0][index + 1] = str.length(); 		
+
+			//Plus operator, long string
+			count = 0;		
+			str = "";
+			current = 0;
+			before = System.currentTimeMillis();
+			while (current <= 1000) {			
+				str += "01234567890123456789012345678901234567890123456789012345678901234567890123456789";
+				count++;
+				current = System.currentTimeMillis() - before;
+			}
+			results[1][index] = count;
+			results[1][index + 1] = str.length();
+
+			//StringBuilder, short string					
+			current = 0;
+			StringBuilder sb1 = new StringBuilder();
+			count = 0;
+			before = System.currentTimeMillis();
+			while (current <= 1000) {			
+				sb1.append("0");
+				count++;
+				current = System.currentTimeMillis() - before;
+				sb1.toString();
+			}	
+			results[2][index] = count;
+			results[2][index + 1] = sb1.length();
+
+			//StringBuilder, long string			
+			current = 0;
+			StringBuilder sb2 = new StringBuilder();
+			count = 0;
+			before = System.currentTimeMillis();		
+			while (current <= 1000) {			
+				sb2.append("01234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				count++;
+				current = System.currentTimeMillis() - before;
+				sb2.toString();
+			}		
+			results[3][index] = count;
+			results[3][index + 1] = sb2.length();		
+		}
+		
+		//Print out final results
+		System.out.println("\n========= CONCATENATION - SHORT STRING =========");
+		System.out.print("Number of concatenations:\t");
+		for (int i = 0; i < RUNS * 2; i += 2) 
+			System.out.print(results[0][i] + " ");		
+		System.out.print("\nFinal string lengths:\t\t");
+		for (int i = 1; i < RUNS * 2; i += 2) 
+			System.out.print(results[0][i] + " ");
+				
+		System.out.println("\n\n========= CONCATENATION - LONG STRING =========");
+		System.out.print("Number of concatenations:\t");
+		for (int i = 0; i < RUNS * 2; i += 2) 
+			System.out.print(results[1][i] + " ");		
+		System.out.print("\nFinal string lengths:\t\t");
+		for (int i = 1; i < RUNS * 2; i += 2) 
+			System.out.print(results[1][i] + " ");
+		
+		System.out.println("\n\n========= STRINGBUILDER - SHORT STRING =========");
+		System.out.print("Number of concatenations:\t");
+		for (int i = 0; i < RUNS * 2; i += 2) 
+			System.out.print(results[2][i] + " ");		
+		System.out.print("\nFinal string lengths:\t\t");
+		for (int i = 1; i < RUNS * 2; i += 2) 
+			System.out.print(results[2][i] + " ");
+		
+		System.out.println("\n\n========= STRINGBUILDER - LONG STRING =========");
+		System.out.print("Number of concatenations:\t");
+		for (int i = 0; i < RUNS * 2; i += 2) 
+			System.out.print(results[3][i] + " ");		
+		System.out.print("\nFinal string lengths:\t\t");
+		for (int i = 1; i < RUNS * 2; i += 2) 
+			System.out.print(results[3][i] + " ");
+	}
+}
--- 1DV507/src/jr222wb_assign4/time/Sorting.java
+++ 1DV507/src/jr222wb_assign4/time/Sorting.java
@@ -1,23 +1,11 @@
-package jr222wb_assign3.sort;
+package jr222wb_assign4.time;
 
-import java.util.Arrays;
-
-public class SortingAlgorithms {
+public class Sorting {
 	public static void main(String[] args) {
-		int[] arr =  { 1, 2, 9, 8, 7, 6, 5, -2 };		
-		System.out.println("INSERTION SORT\nUnsorted:\t" + Arrays.toString(arr));
-		insertionSort(arr);
-		System.out.println("Sorted: \t" + Arrays.toString(insertionSort(arr)));
-		System.out.println("Array intact:\t" + Arrays.toString(arr));
-
-		int[] arr2 =  { 1, 2, 9, 8, 7, 6, 5, -2 };
-		System.out.println("\nMERGE SORT\nUnsorted:\t" + Arrays.toString(arr2));		
-		System.out.println("Sorted: \t" + Arrays.toString(mergeSort(arr2)));
-		System.out.println("Array intact:\t" + Arrays.toString(arr2));
+		
 	}
-
-	//Based on description from https://www.khanacademy.org/computing/computer-science/algorithms/insertion-sort/a/insertion-sort
-	public static int[] insertionSort(int[] in) {
+	
+	public static int[] intInsertionSort(int[] in) {
 		int[] sorted = new int[in.length];
 		System.arraycopy(in, 0, sorted, 0, in.length); //Make copy of array
 		for (int i = 1; i < in.length; i++) { //Iterate over array
@@ -35,23 +23,15 @@ public class SortingAlgorithms {
 		return sorted;
 	}
 	
-	//Only makes a copy of original array and sends to "helper" method
-	public static int[] mergeSort(int[] in) {
-		int[] sorted = new int[in.length];
-		System.arraycopy(in, 0, sorted, 0, in.length);		
-		return mergeSortHelper(sorted);
-	}
-	
-	//Based on description in slides and example in book
-	private static int[] mergeSortHelper(int[] in) {		
+	private static int[] intMergeSort(int[] in) {		
 		if (in.length == 1) //After splitting to single elements, simply return them
 			return in;		
 		int[] left = new int[in.length / 2]; //Otherwise split into two arrays
 		int[] right = new int[in.length - left.length];
 		System.arraycopy(in, 0, left, 0, left.length);
 		System.arraycopy(in, left.length, right, 0, right.length);
-		mergeSortHelper(left); //And recursively run method on each array
-		mergeSortHelper(right);
+		intMergeSort(left); //And recursively run method on each array
+		intMergeSort(right);
 		
 		//Merging		
 		int i = 0, li = 0, ri = 0; //Indices used for merging
@@ -67,4 +47,6 @@ public class SortingAlgorithms {
 			in[i++] = right[ri++];		
 		return in; //Return merged array
 	}	
+
+
 }
