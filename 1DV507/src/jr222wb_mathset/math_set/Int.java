57126505f99295203f0cd13bae225609647e3eab
jr222wb <jr222wb@VD1170-07.lnu.se>
2018-03-20 17:21:25 +0100

    Hoppsan

---
+++ 1DV507/src/jr222wb_mathset/math_set/Int.java
@@ -0,0 +1,37 @@
+/*
+ * File: Int.java
+ * Author: Jonas Lundberg
+ * Date: 20 mar 2014
+ */
+package jr222wb_mathset.math_set;
+
+/**
+ * A simple Int class used in TestMathSet to find certain errors 
+ * related to using the hashCode as element identifier.
+ * 
+ * @author jlnmsi
+ *
+ */
+public class Int {
+	private final int val;
+	
+	public Int(int value) { val = value;}
+	
+	@Override
+	public int hashCode() {return 1;}   // All integers have the same hashCode!
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Int) {
+			Int other = (Int) o;
+			return val == other.val;
+		}
+		else
+			return false;
+	}
+	
+	@Override
+	public String toString() {
+		return ""+val;
+	}
+}
